4c02f7725d38c358d9cc8d0d4b1650228dcbfffc
Jovansonlee Cesar <ivanceras@gmail.com>
2018-07-09 05:03:26 +0800

    Checking localStorage availability

--- static/app.js
+++ static/app.js
@@ -1,9 +1,16 @@
-// the db_url and grouped is retrieved from the indexed db
 function init(){
-    var username = localStorage.getItem('username');
-    var password = localStorage.getItem('password');
-    console.log("username:", username);
-    console.log("password:", password);
+    var username = null;
+    var password = null;
+    var hasStorage = storageAvailable('localStorage');
+    if (hasStorage){
+        username = localStorage.getItem('username');
+        password = localStorage.getItem('password');
+        console.log("username:", username);
+        console.log("password:", password);
+    }
+    else{
+        console.error("localStorage is not supported");
+    }
     var cred = null;
     if (username != null && password != null) {
         cred = {username: username, password: password}
@@ -22,10 +29,45 @@ function init(){
     });
 
     app.ports.setUsername.subscribe(function(username) {
-        localStorage.setItem('username', username);
+        if (hasStorage){
+            localStorage.setItem('username', username);
+        }
+        else{
+            console.error("localStorage is not supported");
+        }
     });
     app.ports.setPassword.subscribe(function(password) {
-        localStorage.setItem('password', password);
+        if (hasStorage){
+            localStorage.setItem('password', password);
+        }
+        else{
+            console.error("localStorage is not supported");
+        }
     });
 }
 window.onload = init
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+function storageAvailable(type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0;
+    }
+}
